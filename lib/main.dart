168afae67a9b67cccd139687889642c9a357d23f
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2021-05-04 22:51:03 +0530

    Initialised the firebase in the application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:rider_app/Screens/LoginScreen.dart';
 import 'package:rider_app/Screens/MainScreen.dart';
 import 'package:rider_app/Screens/RegisterScreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
+
+DatabaseReference usersRef =
+    FirebaseDatabase.instance.reference().child("users");
 
 class MyApp extends StatelessWidget {
   @override
