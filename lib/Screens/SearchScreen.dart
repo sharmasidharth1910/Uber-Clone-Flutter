621f2fde270466d63bb54dc509ec659c33914009
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2021-05-08 15:11:06 +0530

    Functional changes in search screen.

--- lib/Screens/SearchScreen.dart
+++ lib/Screens/SearchScreen.dart
@@ -3,8 +3,10 @@ import 'package:provider/provider.dart';
 import 'package:rider_app/Assistants/requestAssistant.dart';
 import 'package:rider_app/Credentials/ConfigMaps.dart';
 import 'package:rider_app/DataHandler/AppData.dart';
+import 'package:rider_app/Models/Address.dart';
 import 'package:rider_app/Models/PlacePredictions.dart';
 import 'package:rider_app/Widgets/Divider.dart';
+import 'package:rider_app/Widgets/ProgressDialog.dart';
 
 class SearchScreen extends StatefulWidget {
   static const screenId = "SearchScreen";
@@ -223,55 +225,92 @@ class PredictionTile extends StatelessWidget {
     this.places,
   }) : super(key: key);
 
+  void getPlaceAddressDetails(String placeId, BuildContext context) async {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) => ProgressDialog(
+              message: "Setting drop off. Please wait..",
+            ));
+    final String placeDetailsUrl =
+        "https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$apiKey";
+
+    var res = await RequestAssistant.getRequest(url: placeDetailsUrl);
+
+    Navigator.pop(context);
+
+    if (res == "failed") {
+      return;
+    }
+    if (res["status"] == "OK") {
+      Address address = Address();
+      address.placeName = res["result"]["name"];
+      address.placeId = placeId;
+      address.latitude = res["result"]["geometry"]["location"]["lat"];
+      address.longitude = res["result"]["geometry"]["location"]["lng"];
+
+      Provider.of<AppData>(context, listen: false).updateDropOffLocationAddress(
+        dropOffAddress: address,
+      );
+      print("Drop Off Address : " + address.placeName);
+
+      Navigator.pop(context, "obtainDirection");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          SizedBox(
-            width: 10.0,
-          ),
-          Row(
-            children: [
-              Icon(Icons.add_location),
-              SizedBox(
-                width: 14.0,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 8.0,
-                    ),
-                    Text(
-                      places.main_text ?? "",
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                    SizedBox(
-                      height: 2.0,
-                    ),
-                    Text(
-                      places.secondary_text ?? "",
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 12.0,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 8.0,
-                    ),
-                  ],
+    return TextButton(
+      onPressed: () {
+        getPlaceAddressDetails(places.place_id, context);
+      },
+      child: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              width: 10.0,
+            ),
+            Row(
+              children: [
+                Icon(Icons.add_location),
+                SizedBox(
+                  width: 14.0,
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            width: 10.0,
-          ),
-        ],
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Text(
+                        places.main_text ?? "",
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                      SizedBox(
+                        height: 2.0,
+                      ),
+                      Text(
+                        places.secondary_text ?? "",
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: 10.0,
+            ),
+          ],
+        ),
       ),
     );
   }
