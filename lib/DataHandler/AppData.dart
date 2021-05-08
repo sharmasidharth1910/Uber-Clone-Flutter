1a31d66d0f2d580bffaf69eef309a31af4463ad2
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2021-05-09 00:02:52 +0530

    Some bug fixes.

--- lib/DataHandler/AppData.dart
+++ lib/DataHandler/AppData.dart
@@ -3,9 +3,15 @@ import 'package:rider_app/Models/Address.dart';
 
 class AppData extends ChangeNotifier {
   Address pickUpLocation;
+  Address dropOffLocation;
 
   void updatePickUpLocationAddress({Address pickUpAddress}) {
     pickUpLocation = pickUpAddress;
+    notifyListeners();
+  }
+
+  void updateDropOffLocationAddress({Address dropOffAddress}) {
+    dropOffLocation = dropOffAddress;
     notifyListeners();
   }
 }
