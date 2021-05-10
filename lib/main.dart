621b308bccd65b7cfcda25ca836e27a7b9af2106
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2021-05-10 13:41:26 +0530

    Finalised the routes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
@@ -25,11 +26,13 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Taxi Rider App',
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(          
+        theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: RegistrationScreen(),
-        initialRoute: MainScreen.screenId,
+        initialRoute: FirebaseAuth.instance.currentUser == null
+            ? LoginScreen.screenId
+            : MainScreen.screenId,
         routes: {
           RegistrationScreen.screenId: (context) => RegistrationScreen(),
           LoginScreen.screenId: (context) => LoginScreen(),
