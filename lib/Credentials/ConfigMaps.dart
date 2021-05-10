838b8242c3951a8655d653071bd0fa39e9dd4052
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2021-05-10 13:49:37 +0530

    Finalised the user side application.

---
+++ lib/Credentials/ConfigMaps.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rider_app/Models/Users.dart';
+
+final String apiKey = "Add your google maps API key here";
+
+User firebaseUser;
+
+Users currentUserInfo;
