46a3a23b7c8e5f1cee613187cf7e0603b9dabe72
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2021-05-15 07:57:12 +0530

    Added maps reverse geocoding API's.

--- lib/Credentials/ConfigMaps.dart
+++ lib/Credentials/ConfigMaps.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:rider_app/Models/Users.dart';
 
-final String apiKey = "Add your google maps API key here";
+final String apiKey = "AIzaSyApu27Cg4BlUX_AsIJ6aM_N4etHTORozUQ";
 
 User firebaseUser;
 
